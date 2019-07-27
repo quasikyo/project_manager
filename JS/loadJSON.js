179348b635c8a1df463cbeb5495daa47635596d7
mr625 <52550481+mr625@users.noreply.github.com>
2019-07-26 22:32:19 -0400

    I changed some stuff

--- JS/loadJSON.js
+++ JS/loadJSON.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const electron = require('electron');
 const { ipcRenderer } = electron;
 
@@ -10,44 +9,69 @@ const lists = {
 	softList: document.querySelector('#software .software-list')
 };
 
-// Render everything to DOM
-renderJSON('project');
-renderJSON('resource');
-renderJSON('software');
-// emptyLists();
+// Inital call
+renderJSON(['project', 'resource', 'software']);
 
 //=== === === HANDLE ALL ACTIONS TO RENDER TO DOM === === ===//
 
-async function renderJSON(type) {
-	let json;
-	if (type === 'project') { json = await getProjects(); }
-	else if (type === 'resource') { json = await getResources(); }
-	else if (type === 'software') { json = await getSoftware(); }
-	else { console.error('Invalid renderJSON type. Accepted types are "project", "resource", "software".'); return; }
+// Render everything
+function renderJSON(types) {
+	types.forEach(async function(type) {
+		let json;
+		if (type === 'project') { json = await getProjects(); }
+		else if (type === 'resource') { json = await getResources(); }
+		else if (type === 'software') { json = await getSoftware(); }
+		else {
+			console.error(
+				'Invalid renderJSON type. Accepted types are "project", "resource", "software".'
+			);
+			return;
+		}
 
-	// Create elements based on json files and add to DOM
-	for (const id in json) {
-		const item = json[id];
-		const article = createArticle(item);
-		if (item.status === 'finished') { lists.finProjs.appendChild(article); }
-		else if (item.status === 'in progress') { lists.ipProjs.appendChild(article); }
-		// Only resources.json has a versions field
-		else if (typeof item.versions === 'object') {
-			article.querySelector('h3.header').appendChild(createStackType(item));
-			lists.resList.appendChild(article);
+		// Create elements based on json files and add to DOM
+		for (const id in json) {
+			const item = json[id];
+			const article = createArticle(item);
+			// for projects.json
+			if (item.status === 'finished') { lists.finProjs.appendChild(article); }
+			else if (item.status === 'in progress') { lists.ipProjs.appendChild(article); }
+			// Only resources.json has a versions field - for resources.json
+			else if (typeof item.versions === 'object') {
+				article.querySelector('h3.header').appendChild(createStackType(item));
+				lists.resList.appendChild(article);
+			}
+			// Same concept as above - for software.json
+			else if (typeof item.usedWithRes === 'object') { lists.softList.appendChild(article); }
 		}
-		// Same concept as above
-		else if (typeof item.usedWithRes === 'object') {
-			lists.softList.appendChild(article);
+	});
+	// Check for empty lists - DOESN'T WORK DUE TO ASYNC/AWAIT; this stuff gets run during the await
+	/* for (const key in lists) {
+		list = lists[key];
+		if (list.childElementCount === 0) {
+			list.appendChild(createArticle(null));
 		}
+	} */
+}
+
+// Render new JSON objects
+function renderNewJSON(newObj) {
+	const article = createArticle(newObj);
+	// for projects.json
+	if (newObj.status === 'finished') { lists.finProjs.appendChild(article); }
+	else if (newObj.status === 'in progress') { lists.ipProjs.appendChild(article); }
+	// Only resources.json has a versions field - for resources.json
+	else if (typeof newObj.versions === 'object') {
+		article.querySelector('h3.header').appendChild(createStackType(newObj));
+		lists.resList.appendChild(article);
 	}
-
+	// Same concept as above - for software.json
+	else if (typeof newObj.usedWithRes === 'object') { lists.softList.appendChild(article); }
 }
 
 //=== === === HANDLE @click === === ===//
 
 function openPage(event) {
-	ipcRenderer.send('open-box', 'whatup');
+	ipcRenderer.send('open-box', 'click');
 }
 
 //=== === === GET JSON === === ===//
@@ -70,19 +94,6 @@ async function getSoftware() {
 	return data;
 }
 
-//=== === === TODO: CHECK FOR EMPTY LISTS === === ===//
-
-//If a list is empty, add a "Nothing found element to it."
-/* function emptyLists() {
-	console.log(lists.finProjs.childElementCount);
-	for (const list in lists) {
-
-		if (lists[list].childElementCount === 0) {
-			lists[list].appendChild(createEmptyArticle());
-		}
-	}
-} */
-
 //=== === === CREATE DOM ELEMENTS === === ===//
 
 function createElem(elem) {
@@ -97,7 +108,7 @@ function createArticle(getTextFor) {
 	// Create h3.header and append
 	const header = createElem('h3');
 	header.className = `header flex just-cont-btwn`;
-	header.textContent = getTextFor.name;
+	header.textContent = getTextFor !== null ? getTextFor.name : 'Nothing Found';
 	article.appendChild(header);
 	// Create div.content and append
 	const content = createElem('div');
@@ -106,30 +117,7 @@ function createArticle(getTextFor) {
 	// create p.desc and append
 	const desc = createElem('p');
 	desc.className = 'desc';
-	desc.textContent = getTextFor.desc
-	content.appendChild(desc);
-
-	return article;
-}
-// Honestly, I could probably combine the two surrounding methods,
-// but no.
-function createEmptyArticle() {
-	// Create article.box, append, and add @click listener
-	const article = createElem('article');
-	article.className = 'box';
-	// Create h3 and append
-	const header = createElem('h3');
-	header.className = `header`;
-	header.textContent = 'Nothing Found';
-	article.appendChild(header);
-	// Create div.content and append
-	const content = createElem('div');
-	content.className = 'content';
-	article.appendChild(content);
-	// create p.desc and append
-	const desc = createElem('p');
-	desc.className = 'desc';
-	desc.textContent = 'This section is empty.';
+	desc.textContent = getTextFor !== null ? getTextFor.desc : 'This section is empty';
 	content.appendChild(desc);
 
 	return article;
